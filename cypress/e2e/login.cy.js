89a103972f84fbd48c2e5b1d5c887a6c1cf82d3a
Marillia Dantas <marillia.dantas@hotmail.com>
2024-11-22 18:07:44 -0300

    teste

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,9 @@
+describe('Orange HTM Tests', () => {
+  it('Login - Success', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    cy.get('.oxd-button').click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+  })
+})
