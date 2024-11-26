d8d43210051bfc3f163f54d5a458ddee45bef98f
Marillia Dantas <marillia.dantas@hotmail.com>
2024-11-26 16:05:24 -0300

    CT: invalid user

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -13,6 +13,6 @@ describe('Orange HTM Tests', () => {
     cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Teste')
     cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
     cy.get('.oxd-button').click()
-    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get('.oxd-alert').should('have.text', "Invalid credentials")
   })
 })
